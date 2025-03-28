63d04fbbd45a05c1891d196df002d7dadd5321fd
ShadoKaiross <shadokairos@gmail.com>
2025-03-28 08:31:13 +0100

    Update main.py
    .

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
 import discord
 from discord.ext import commands
 import os
-from flask import Flask
-import threading
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+from keep_alive import keep_alive
 
-@app.route('/')
-def index():
-    return "Bot is running!"
+keep_alive()  # Web sunucusunu başlat
 
-def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+load_dotenv()  # .env dosyasını yükle
+TOKEN = os.getenv('TOKEN')  # GitHub secrets'tan bot token'ını al
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True  # Mesaj içeriğini okuma izni
 
-def run_bot():
-    TOKEN = os.getenv("TOKEN")  # Bot token'ını Replit'ten alıyoruz
-    intents = discord.Intents.default()
-    intents.messages = True  # Mesajları okuma izni
-    intents.guilds = True  # Sunucuları izleme izni
-    intents.message_content = True  # Mesaj içeriğini okuma izni
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptı!')
 
-    @bot.event
-    async def on_ready():
-        print(f'{bot.user} olarak giriş yaptı!')
+# !temizle komutu
+@bot.command()
+async def temizle(ctx, channel_name: str):
+    # Kanal adı ile kanalı bulma
+    channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
 
-    @bot.command()
-    async def temizle(ctx, channel_name: str):
-        channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
+    if not channel:
+        await ctx.send(f"{channel_name} adlı kanal bulunamadı!")
+        return
 
-        if not channel:
-            await ctx.send(f"{channel_name} adlı kanal bulunamadı!")
-            return
+    # Kanalın geçmişindeki mesajları silme
+    await ctx.send(f"{channel_name} adlı kanal temizleniyor...")
 
-        await ctx.send(f"{channel_name} adlı kanal temizleniyor...")
-        
-        # 300 mesajı silmek için döngü ile purge işlemi
+    # 300 mesajı silmek için döngü ile purge işlemi
     limit = 300  # Her seferinde silmek istediğiniz mesaj sayısı
     while True:
         deleted = await channel.purge(limit=100)  # Bir seferde maksimum 100 siler
@@ -46,13 +43,7 @@ def run_bot():
         if limit <= 0:  # Hedeflenen mesaj sayısına ulaşıldıysa dur
             break
 
-        await ctx.send(f"{channel_name} adlı kanal başarıyla temizlendi!")
+    # Temizleme işlemi tamamlandıktan sonra bilgilendirme
+    await ctx.send(f"{channel_name} adlı kanal başarıyla temizlendi!")
 
-    bot.run(TOKEN)
-
-if __name__ == '__main__':
-    # Flask web server'ını başlatmak için bir thread başlatıyoruz
-    threading.Thread(target=run_flask).start()
-    
-    # Botu başlatıyoruz
-    run_bot()
+bot.run(TOKEN)
