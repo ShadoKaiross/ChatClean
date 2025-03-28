dee75ac4c47ce316675dad0ea8627e81148aba90
ShadoKaiross <shadokairos@gmail.com>
2025-03-28 03:26:50 +0100

    Create bot.py
    bot kodu

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+
+
+TOKEN = os.getenv("TOKEN")  # Bot token'ını Replit'ten alıyoruz
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True  # Mesaj içeriğini okuma izni
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptı!')
+
+# !temizle komutu
+@bot.command()
+async def temizle(ctx, channel_name: str):
+    # Kanal adı ile kanalı bulma
+    channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
+
+    if not channel:
+        await ctx.send(f"{channel_name} adlı kanal bulunamadı!")
+        return
+
+    # Kanalın geçmişindeki tüm mesajları silme
+    await ctx.send(f"{channel_name} adlı kanal temizleniyor...")
+
+    # Mesajları temizleme işlemi
+    await channel.purge()
+
+    # Temizleme işlemi tamamlandıktan sonra bilgilendirme
+    await ctx.send(f"{channel_name} adlı kanal başarıyla temizlendi!")
+
+bot.run(TOKEN)
